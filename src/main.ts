dbda2b3d7a2de5b737b1cb4d13bf6a30a0da1c8a
otowmoyarng <hidemusiam@gmail.com>
2023-11-16 00:02:45 +0900

    cssとjsファイルを分離する

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 function doGet(): GoogleAppsScript.HTML.HtmlOutput {
     const htmlTemplate = HtmlService.createTemplateFromFile('src/index.html');
+    htmlTemplate.style = include('src/style.css');
+    htmlTemplate.script = include('src/script.js');
     return htmlTemplate.evaluate();
+}
+
+function include(filename:string): string {
+    return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
